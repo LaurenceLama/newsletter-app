1b2570e33cbeb7654358b46e621d5219090acd3d
Laurence Lama <laurencelamaFSE@gmail.com>
2024-07-05 23:08:14 +0800

    Initializing newsletter UI progress

---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        title: "title 3s ease-out forwards",
+      },
+      keyframes: {
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25em",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0%",
+          },
+          "80%": {
+            opacity: "100%",
+          },
+
+          "100%": {
+            "line-height": "100%",
+            opacity: "100%",
+          },
+        },
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
